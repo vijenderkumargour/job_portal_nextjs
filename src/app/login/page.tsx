2096508349cb1ec493eaa2484ee0d8eb347dbbe1
vijender kumar <vijenderkumargour@gamil.com>
2025-11-27 21:39:51 +0530

    change login and registration page in folder structure

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,143 +1,20 @@
-"use client";
+import LoginForm from "@/features/auth/components/login-form";
+import { getCurrentUser } from "@/features/auth/server/auth.queries";
+import { redirect } from "next/navigation";
 
-import React, { ChangeEvent, FormEvent, useState } from "react";
-import { Input } from "@/components/ui/input";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Eye, EyeOff, Lock, Mail, UserCheck } from "lucide-react";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { loginUserAction } from "@/features/auth/server/auth.actions";
-import { toast } from "sonner";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { LoginUserData, loginUserSchema } from "@/features/auth/auth.schema";
-import { useRouter } from "next/navigation";
+const LoginPage = async () => {
+  const user = await getCurrentUser();
 
-const LoginForm: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm({
-    resolver: zodResolver(loginUserSchema),
-  });
-
-  const [showPassword, setShowPassword] = useState(false);
-  const router = useRouter();
-  const onSubmit = async (data: LoginUserData) => {
-    try {
-      const result = await loginUserAction(data);
-      if (result.status === "SUCCESS") {
-        if (result.role === "employer") router.push("/employer-dashboard");
-        else router.push("/dashboard");
-      }
-      if (result.status === "SUCCESS") toast.success(result.message);
-      else toast.error(result.message);
-    } catch (error) {}
-  };
+  if (user) {
+    if (user.role === "applicant") return redirect("/dashboard");
+    if (user.role === "employer") return redirect("/employer-dashboard");
+  }
 
   return (
-    <div className="min-h-screen bg-background flex items-center justify-center p-4">
-      <Card className="w-full max-w-md">
-        <CardHeader className="text-center">
-          <div className="mx-auto w-16 h-16 bg-primary rounded-full flex items-center justify-center mb-4">
-            <UserCheck className="w-8 h-8 text-primary-foreground" />
-          </div>
-          <CardTitle className="text-2xl">Join Our Job Portal</CardTitle>
-          <CardDescription>Create your account to get started</CardDescription>
-        </CardHeader>
-
-        <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-            {/* Email Field */}
-            <div className="space-y-2">
-              <Label htmlFor="email">Email Address *</Label>
-              <div className="relative">
-                <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-muted-foreground" />
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="Enter your email"
-                  required
-                  {...register("email")}
-                  className={`pl-10 ${
-                    errors.email ? "border-destructive" : ""
-                  }`}
-                />
-              </div>
-              {errors.email && (
-                <p className="text-sm text-destructive">
-                  {errors.email.message}
-                </p>
-              )}
-            </div>
-
-            {/* Password Field */}
-            <div className="space-y-2">
-              <Label htmlFor="password">Password *</Label>
-              <div className="relative">
-                <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-muted-foreground" />
-                <Input
-                  id="password"
-                  type={showPassword ? "text" : "password"}
-                  placeholder="Create a strong password"
-                  required
-                  {...register("password")}
-                  className={`pl-10 ${
-                    errors.password ? "border-destructive" : ""
-                  }`}
-                />
-
-                <Button
-                  type="button"
-                  variant="ghost"
-                  size="sm"
-                  className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? (
-                    <EyeOff className="w-4 h-4 text-muted-foreground" />
-                  ) : (
-                    <Eye className="w-4 h-4 text-muted-foreground" />
-                  )}
-                </Button>
-              </div>
-              {errors.password && (
-                <p className="text-sm text-destructive">
-                  {errors.password.message}
-                </p>
-              )}
-            </div>
-
-            {/* Submit Button */}
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-
-            <div className="text-center">
-              <p className="text-sm text-muted-foreground">
-                Don't have an account?
-                <Link
-                  href="/register"
-                  className="text-primary hover:text-primary/80 font-medium underline-offset-4 hover:underline"
-                >
-                  &nbsp; Create Account
-                </Link>
-              </p>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+    <>
+      <LoginForm />
+    </>
   );
 };
 
-export default LoginForm;
+export default LoginPage;
